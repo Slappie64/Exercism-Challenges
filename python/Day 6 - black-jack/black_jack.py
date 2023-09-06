9067ebce6eeacbb92643548f9b1015c967b8eb0c
Luigi Marino <emails@luigi-marino.com>
2023-09-06 13:45:42 +0100

    Day 6 - blackjack - Challenge Completed

--- python/Day 6 - black-jack/black_jack.py
+++ python/Day 6 - black-jack/black_jack.py
@@ -4,7 +4,6 @@ How to play blackjack:    https://bicyclecards.com/how-to-play/blackjack/
 "Standard" playing cards: https://en.wikipedia.org/wiki/Standard_52-card_deck
 """
 
-# TODO: This is a test todo
 def value_of_card(card):
     """Determine the scoring value of a card.
 
@@ -16,7 +15,12 @@ def value_of_card(card):
     3.  '2' - '10' = numerical value.
     """
 
-    pass
+    if card in ('K', 'Q', 'J'):
+        return 10
+    if card == 'A':
+        return 1
+    
+    return int(card)
 
 
 def higher_card(card_one, card_two):
@@ -29,8 +33,15 @@ def higher_card(card_one, card_two):
     2.  'A' (ace card) = 1
     3.  '2' - '10' = numerical value.
     """
+    card_one_value = value_of_card(card_one)
+    card_two_value = value_of_card(card_two)
 
-    pass
+    if card_one_value == card_two_value:
+        return card_one, card_two
+    if card_one_value > card_two_value:
+        return card_one
+
+    return card_two
 
 
 def value_of_ace(card_one, card_two):
@@ -43,8 +54,24 @@ def value_of_ace(card_one, card_two):
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
     """
+    card_one_value = value_of_card(card_one)
+    card_two_value = value_of_card(card_two)
+    card_total = 0
 
-    pass
+    if card_one_value == 1:
+        card_one_value += 10
+        card_total = card_one_value + card_two_value
+    elif card_two_value == 1:
+        card_two_value += 10
+        card_total = card_one_value + card_two_value
+
+    else:
+        card_total = card_one_value + card_two_value
+    
+    if card_total < 11:
+        return 11
+
+    return 1
 
 
 def is_blackjack(card_one, card_two):
@@ -57,8 +84,12 @@ def is_blackjack(card_one, card_two):
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
     """
+    if card_one == 'A':
+        return 11 + value_of_card(card_two) == 21
+    if card_two == 'A':
+        return value_of_card(card_one) + 11 == 21
 
-    pass
+    return value_of_card(card_one) + value_of_card(card_two) == 21
 
 
 def can_split_pairs(card_one, card_two):
@@ -68,7 +99,7 @@ def can_split_pairs(card_one, card_two):
     :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
     """
 
-    pass
+    return value_of_card(card_one) == value_of_card(card_two)
 
 
 def can_double_down(card_one, card_two):
@@ -78,4 +109,4 @@ def can_double_down(card_one, card_two):
     :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
     """
 
-    pass
+    return value_of_card(card_one) + value_of_card(card_two) >= 9 and value_of_card(card_one) + value_of_card(card_two) < 12
