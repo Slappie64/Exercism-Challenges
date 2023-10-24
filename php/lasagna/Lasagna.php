9a04b103ae2572bc7db480a1a7a80b35a867ce26
Luigi Marino <emails@luigi-marino.com>
2023-10-24 12:10:28 +0100

    PHP - Lasagna - Challenge Complete

--- php/lasagna/Lasagna.php
+++ php/lasagna/Lasagna.php
@@ -5,12 +5,28 @@ declare(strict_types=1);
 class Lasagna
 {
     // Please define the 'expectedCookTime()' function
-
+    function expectedCookTime()
+    {
+        return 40;
+    }
     // Please define the 'remainingCookTime($elapsed_minutes)' function
-
-    // Please define the 'totalPreparationTime($layers_to_prep)' function
-
-    // Please define the 'totalElapsedTime($layers_to_prep, $elapsed_minutes)' function
-
+    function remainingCookTime($elapsed_minutes)
+    {
+        return $this->expectedCookTime() - $elapsed_minutes;
+    }
+    // Please define the 'totalPreparationTime($layers_to_prep_to_prep)' function
+    function totalPreparationTime($layers_to_prep_to_prep)
+    {
+        return $layers_to_prep_to_prep * 2;
+    }
+    // Please define the 'totalElapsedTime($layers_to_prep_to_prep, $elapsed_minutes)' function
+    function totalElapsedTime($layers_to_prep, $elapsed_minutes)
+    {
+        return $this->totalPreparationTime($layers_to_prep)+$elapsed_minutes;
+    }
     // Please define the 'alarm()' function
+    function alarm()
+    {
+        return "Ding!";
+    }
 }
