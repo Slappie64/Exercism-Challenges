01da11fa589b2ef73cd9f88cf3fbfbb4c33a2215
Luigi Marino <emails@luigi-marino.com>
2023-11-08 15:28:35 +0000

    C# - 02 Infiltration - Challenge Completed

---
+++ csharp/02 - annalyns-infiltration/AnnalynsInfiltration.cs
@@ -0,0 +1,15 @@
+using System;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
+    
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) => knightIsAwake || archerIsAwake || prisonerIsAwake;
+
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) => !archerIsAwake && prisonerIsAwake;
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent) => (petDogIsPresent && !archerIsAwake) || (!knightIsAwake && !archerIsAwake && prisonerIsAwake);
+
+}
