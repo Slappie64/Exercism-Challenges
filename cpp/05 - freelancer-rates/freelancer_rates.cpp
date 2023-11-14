fd58bca33d7547c1c20b078638eeec7458f8a58e
Luigi Marino <emails@luigi-marino.com>
2023-11-14 13:33:47 +0000

    C++ - 05 Freelancer Rates - Challenge Completed

--- cpp/05 - freelancer-rates/freelancer_rates.cpp
+++ cpp/05 - freelancer-rates/freelancer_rates.cpp
@@ -1,32 +1,26 @@
 // INFO: Headers from the standard library should be inserted at the top via
 // #include <LIBRARY_NAME>
+#include <math.h>
 
 // daily_rate calculates the daily rate given an hourly rate
 double daily_rate(double hourly_rate) {
-    // TODO: Implement a function to calculate the daily rate given an hourly
-    // rate
-    return 0.0;
+    return hourly_rate * 8;
 }
 
 // apply_discount calculates the price after a discount
 double apply_discount(double before_discount, double discount) {
-    // TODO: Implement a function to calculate the price after a discount.
-    return 0.0;
+    return before_discount - (before_discount * (discount / 100));
 }
 
 // monthly_rate calculates the monthly rate, given an hourly rate and a discount
 // The returned monthly rate is rounded up to the nearest integer.
 int monthly_rate(double hourly_rate, double discount) {
-    // TODO: Implement a function to calculate the monthly rate, and apply a
-    // discount.
-    return 0;
+    return std::ceil(apply_discount(daily_rate(hourly_rate), discount) * 22);
 }
 
 // days_in_budget calculates the number of workdays given a budget, hourly rate,
 // and discount The returned number of days is rounded down (take the floor) to
 // the next integer.
 int days_in_budget(int budget, double hourly_rate, double discount) {
-    // TODO: Implement a function that takes a budget, an hourly rate, and a
-    // discount, and calculates how many complete days of work that covers.
-    return 0;
+    return std::floor(budget / apply_discount(daily_rate(hourly_rate), discount));
 }
