8896422bd8dedca785e05ae21c54112d7c867969
Luigi Marino <emails@luigi-marino.com>
2023-11-15 13:42:29 +0000

    C# - 07 While Loops - Challenge Downloaded

---
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -0,0 +1,24 @@
+using System;
+
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance)
+    {
+        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
+    }
+
+    public static decimal Interest(decimal balance)
+    {
+        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
+    }
+
+    public static decimal AnnualBalanceUpdate(decimal balance)
+    {
+        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
+    }
+
+    public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+    {
+        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+    }
+}
