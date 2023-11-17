9f47a3cc4d0706881744dbfca17e9941535e4964
Luigi Marino <emails@luigi-marino.com>
2023-11-17 13:00:26 +0000

    CPP - 07 Interest - Challenge completed.

--- cpp/07 - interest-is-interesting/interest_is_interesting.cpp
+++ cpp/07 - interest-is-interesting/interest_is_interesting.cpp
@@ -1,25 +1,41 @@
 // interest_rate returns the interest rate for the provided balance.
 double interest_rate(double balance) {
-    // TODO: Implement the interest_rate function
-    return 0.0;
+    if (balance < 0) {
+        return 3.213;
+    }
+    else if (balance >= 0 && balance < 1000) {
+        return 0.5;
+    }
+    else if (balance >= 1000 && balance < 5000) {
+        return 1.621;
+    }
+    else if (balance >= 5000) {
+        return 2.475;
+    }
+    else {
+        return balance;
+    }
 }
 
 // yearly_interest calculates the yearly interest for the provided balance.
 double yearly_interest(double balance) {
-    // TODO: Implement the yearly_interest function
-    return 0.0;
+    return balance * (interest_rate(balance)/100);
 }
 
 // annual_balance_update calculates the annual balance update, taking into
 // account the interest rate.
 double annual_balance_update(double balance) {
     // TODO: Implement the annual_balance_update function
-    return 0.0;
+    return balance + yearly_interest(balance);
 }
 
 // years_until_desired_balance calculates the minimum number of years required
 // to reach the desired balance.
 int years_until_desired_balance(double balance, double target_balance) {
-    // TODO: Implement the years_until_desired_balance function
-    return 0;
+    int years = 0;
+    while (balance < target_balance) {
+        balance = annual_balance_update(balance);
+        years += 1;
+    }
+    return years;
 }
