c892fc9c9afb406c00db56da5bf681ee7e301e1e
Luigi Marino <emails@luigi-marino.com>
2023-11-22 15:39:46 +0000

    C# - 08 - Marketing - Challenge Completed

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-static class Badge
-{
-    public static string Print(int? id, string name, string? department)
-    {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
-    }
-}
