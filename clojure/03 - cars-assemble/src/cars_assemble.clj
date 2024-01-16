b81ea859cd3df6cc90bce69ed9e5e24ec35759fa
Luigi Marino <emails@luigi-marino.com>
2024-01-16 15:26:33 +0000

    Clojure - 03 - Cars Assemble - Challenge Completed

--- clojure/03 - cars-assemble/src/cars_assemble.clj
+++ clojure/03 - cars-assemble/src/cars_assemble.clj
@@ -1,12 +1,26 @@
 (ns cars-assemble)
 
+(def cars-per-hour 221)
+
+(defn success-rate [speed]
+  (condp <= speed
+    10 0.77
+    9 0.8
+    5 0.9
+    1 1.0
+    0.0)
+)
+
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  )
+  (* speed cars-per-hour (success-rate speed))
+)
+
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
-  )
+  (int (/ (production-rate speed) 60))
+)
