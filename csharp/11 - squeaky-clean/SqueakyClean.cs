e982646a88b6599b5d9cbf2f4c968ebd73296dc5
Luigi Marino <emails@luigi-marino.com>
2024-01-24 14:56:20 +0000

    C# - 11 - Squeaky Clean - Challenge Complete

---
+++ csharp/11 - squeaky-clean/SqueakyClean.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+public static class Identifier
+{
+    private static bool IsGreekLowercase(char c) => (c >= 'α' && c <= 'ω');
+    
+    public static string Clean(string identifier)
+    {
+        var stringBuilder = new StringBuilder();
+        var isAfterDash = false;
+        foreach (var c in identifier)
+        {
+            stringBuilder.Append(c switch
+            {
+                _ when IsGreekLowercase(c) => default,
+                _ when isAfterDash => char.ToUpperInvariant(c),
+                _ when char.IsWhiteSpace(c) => '_',
+                _ when char.IsControl(c) => "CTRL",
+                _ when char.IsLetter(c) => c,
+                _ => default,
+            });
+            isAfterDash = c.Equals('-');
+        }
+        return stringBuilder.ToString();
+    }
+}
--- csharp/squeaky-clean/SqueakyClean.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class Identifier
-{
-    public static string Clean(string identifier)
-    {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
-    }
-}
