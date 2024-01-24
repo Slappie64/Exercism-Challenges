46aa216fd01a1b256d14b7fb814a70c8bfcda1da
Luigi Marino <emails@luigi-marino.com>
2024-01-24 14:57:12 +0000

    C# - 12 - NeedForSpeed - Challenge Downloaded

---
+++ csharp/need-for-speed/NeedForSpeed.cs
@@ -0,0 +1,36 @@
+using System;
+
+class RemoteControlCar
+{
+    // TODO: define the constructor for the 'RemoteControlCar' class
+
+    public bool BatteryDrained()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
+    }
+
+    public int DistanceDriven()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
+    }
+
+    public void Drive()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+    }
+
+    public static RemoteControlCar Nitro()
+    {
+        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
+    }
+}
+
+class RaceTrack
+{
+    // TODO: define the constructor for the 'RaceTrack' class
+
+    public bool TryFinishTrack(RemoteControlCar car)
+    {
+        throw new NotImplementedException("Please implement the RaceTrack.TryFinishTrack() method");
+    }
+}
