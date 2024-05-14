f8b58233f7a874bf55cc159afc97ddc1a44a699a
Luigi Marino <emails@luigi-marino.com>
2024-05-14 11:21:14 +0100

    Organizing folder structure for learning projects

--- csharp/00 - hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ csharp/00 - hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f7dbeadda051499b3bca37a7e73a8480eb798f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5220b884c41a4bdf9eb02c32c1b049d9207d63")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
