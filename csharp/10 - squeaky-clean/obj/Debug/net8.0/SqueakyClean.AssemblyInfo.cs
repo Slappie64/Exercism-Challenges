835f99aa8ba6ea999db470b80b3e2ebf28937fbf
Luigi Marino <emails@luigi-marino.com>
2024-05-22 12:41:34 +0100

    C# - 22 - AttackOfTrolls - Challenge Completed

--- csharp/10 - squeaky-clean/obj/Debug/net8.0/SqueakyClean.AssemblyInfo.cs
+++ csharp/10 - squeaky-clean/obj/Debug/net8.0/SqueakyClean.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SqueakyClean")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+580bf735ae59448ab67b9bf1e3273efc8b9ceeca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ecc9db1a9790cda13eee46701a3102505950fa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SqueakyClean")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SqueakyClean")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- csharp/14 - tim-from-marketing/obj/Debug/net8.0/TimFromMarketing.AssemblyInfo.cs
+++ csharp/14 - tim-from-marketing/obj/Debug/net8.0/TimFromMarketing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimFromMarketing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+580bf735ae59448ab67b9bf1e3273efc8b9ceeca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ecc9db1a9790cda13eee46701a3102505950fa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimFromMarketing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimFromMarketing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
