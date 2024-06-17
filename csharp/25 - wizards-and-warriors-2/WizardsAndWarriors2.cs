5241061b40bee30e26d94ad5e90ae9887c92e145
Luigi Marino <emails@luigi-marino.com>
2024-06-17 12:12:56 +0100

    C# - 25 - Wizards And Warriors 2 - Challenge Complete

---
+++ csharp/25 - wizards-and-warriors-2/WizardsAndWarriors2.cs
@@ -0,0 +1,41 @@
+using System;
+
+static class GameMaster
+{
+    public static string Describe(Character character) => $"You're a level {character.Level} {character.Class} with {character.HitPoints} hit points.";
+
+    public static string Describe(Destination destination) => $"You've arrived at {destination.Name}, which has {destination.Inhabitants} inhabitants.";
+    
+
+    public static string Describe(TravelMethod travelMethod) =>
+    travelMethod switch
+    {
+        
+        TravelMethod.Walking => "You're traveling to your destination by walking.",
+        TravelMethod.Horseback => "You're traveling to your destination on horseback."
+    };
+    
+
+    public static string Describe(Character character, Destination destination, TravelMethod travelMethod) => $"{Describe(character)} {Describe(travelMethod)} {Describe(destination)}";
+    
+    public static string Describe(Character character, Destination destination)=> $"{Describe(character)} {Describe(TravelMethod.Walking)} {Describe(destination)}";
+}
+
+class Character
+{
+    public string Class { get; set; }
+    public int Level { get; set; }
+    public int HitPoints { get; set; }
+}
+
+class Destination
+{
+    public string Name { get; set; }
+    public int Inhabitants { get; set; }
+}
+
+enum TravelMethod
+{
+    Walking,
+    Horseback
+}
---
+++ csharp/remote-control-competition/RemoteControlCompetition.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+// TODO implement the IRemoteControlCar interface
+
+public class ProductionRemoteControlCar
+{
+    public int DistanceTravelled { get; private set; }
+    public int NumberOfVictories { get; set; }
+
+    public void Drive()
+    {
+        DistanceTravelled += 10;
+    }
+}
+
+public class ExperimentalRemoteControlCar
+{
+    public int DistanceTravelled { get; private set; }
+
+    public void Drive()
+    {
+        DistanceTravelled += 20;
+    }
+}
+
+public static class TestTrack
+{
+    public static void Race(IRemoteControlCar car)
+    {
+        throw new NotImplementedException($"Please implement the (static) TestTrack.Race() method");
+    }
+
+    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
+        ProductionRemoteControlCar prc2)
+    {
+        throw new NotImplementedException($"Please implement the (static) TestTrack.GetRankedCars() method");
+    }
+}
